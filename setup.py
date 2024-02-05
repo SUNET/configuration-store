abd711f8ff1b1d4f8711a5eb384dd1a65563b551
Johannes Jeppsson <2286858+YotaYota@users.noreply.github.com>
2024-02-05 10:57:56 +0100

    update setup.py with req

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from glob import glob
+import pathlib
 from os.path import basename, splitext
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="confstore",
-    version="0.0.1",
+    version="0.1.0",
     author="Kristofer Hallin, Johannes Jeppsson",
     author_email="clixon-pyapi@8n1.se",
     description="Config store through git",
-    long_description=long_description,
+    long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/SUNET/configuration-store",
     package_dir={"": "src"},
@@ -24,4 +22,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
+    install_requires=["GitPython>=3.1.41"]
 )
